b3a9c54a026728846f42e60be6ac532e44503706
Kure-ru <clairedochez@gmail.com>
2023-01-19 18:09:01 +0900

    finished 1st version of calculator. Calculator is now able to display basic operations when pressing = . TO DO : fix bugs, decimals, improve appearance, backspace button, keyboard support

--- script.js
+++ script.js
@@ -1,29 +1,23 @@
 const display = document.getElementById("display");
-const buttons = document.querySelectorAll('button')
+const numButtons = document.querySelectorAll('button.number')
+const operatorButtons = document.querySelectorAll('button.operator')
+let array = []
 
-//addition
-const add = (a, b) => {
-    return a + b;
+let operation = {
+    num1 : 0,
+    num2 : 0,
+    operator: null
 }
 
-//subtraction
-const subtract = (a, b) => {
-    return a - b;
-}
-
-//multiplication
-const multiply = (a, b) => {
-    return a * b;
-}
-
-//division
-const divide = (a, b) => {
-    return a / b;
-}
+//operations
+const add = (a, b) => {return a + b};
+const subtract = (a, b) => { return a - b};
+const multiply = (a, b) => { return a * b};
+const divide = (a, b) => {return a / b};
 
 //Create a new function operate that takes an operator and
 //2 numbers and then calls one of the above functions on the numbers.
-const operate = (operator, num1, num2) => {
+const operate = (num1, num2, operator) => {
    switch (operator) {
     case "+" :
         return add(num1, num2);
@@ -36,22 +30,49 @@ const operate = (operator, num1, num2) => {
    }
 }
 
-//change display
-const changeDisplay = (displayValue) => {
-    console.log(displayValue)
-    display.innerHTML = displayValue;
-}
+//reset button
+const resetButton =  document.querySelector("#CE");
+resetButton.addEventListener('click', (event) => {
+    display.innerHTML = "";
+    array = []
+})
 
-//find id of button 
-let value = button.id
+//equal button 
+const equalButton = document.querySelector("#equal");
+equalButton.addEventListener('click', (event) => {
+    if (array !== []) {
+        operation.num2 = Number(array.join(""));
+        array = [];
+        console.log(operation)
+        let result = operate(operation.num1, operation.num2, operation.operator)
+        console.log(result);
+        display.innerHTML = result
+        return result;
+    }
+})
 
-//populate display when pushing button
-    buttons.forEach((button) => {
-    button.addEventListener('click', changeDisplay)
-    })
+//input number
+ numButtons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+            display.innerHTML += button.id;
+            // if a number, add it to operation
+            array.push(button.id)
+            console.log(array)
+            // if an operand, stop
+    })})
 
+//input operator and update 1st value
+operatorButtons.forEach((button) => {
+    button.addEventListener('click',(event) => {
+        if (array !== []) {
+            operation.num1 = Number(array.join(""));
+            array = [];
+            display.innerHTML = ""
+            operation.operator = button.id
+            console.log(operation.num1)
+            console.log(operation.operator)
+        }
+    } )
+})
 
-
-//store value of id button *2
-
-//return result
+//
