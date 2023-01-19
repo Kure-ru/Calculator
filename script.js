01472ff3ef546fb986d42c4e4e9f521a15ec3033
Kure-ru <clairedochez@gmail.com>
2023-01-19 21:01:52 +0900

    refactored code. Added function to round answers with long decimals.

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const operatorButtons = document.querySelectorAll('button.operator')
 let array = []
 
 let operation = {
-    num1 : 0,
-    num2 : 0,
-    operator: null
+    num1 : '',
+    num2 : '',
+    operator: ''
 }
 
 //operations
@@ -14,6 +14,10 @@ const add = (a, b) => {return a + b};
 const subtract = (a, b) => { return a - b};
 const multiply = (a, b) => { return a * b};
 const divide = (a, b) => {return a / b};
+
+const roundResult = (number)  => {
+    return Math.round(number * 1000) / 1000
+  }
 
 //Create a new function operate that takes an operator and
 //2 numbers and then calls one of the above functions on the numbers.
@@ -33,46 +37,66 @@ const operate = (num1, num2, operator) => {
 //reset button
 const resetButton =  document.querySelector("#CE");
 resetButton.addEventListener('click', (event) => {
-    display.innerHTML = "";
-    array = []
+   resetData();  
 })
 
-//equal button 
-const equalButton = document.querySelector("#equal");
-equalButton.addEventListener('click', (event) => {
-    if (array !== []) {
-        operation.num2 = Number(array.join(""));
-        array = [];
-        console.log(operation)
-        let result = operate(operation.num1, operation.num2, operation.operator)
-        console.log(result);
-        display.innerHTML = result
-        return result;
-    }
-})
+//reset data
+const resetData = () => {
+    array = [];
+    display.innerHTML = ""
+    Object.keys(operation).forEach(key => {
+        operation[key] = '';
+    });
+    console.log(operation)
+}
+
+//heart button 
+const heartButton = document.querySelector("#♡");
+heartButton.addEventListener('click', (event) => {
+   display.innerHTML = "HI THERE!"
+    })
 
 //input number
  numButtons.forEach((button) => {
     button.addEventListener('click', (event) => {
-            display.innerHTML += button.id;
-            // if a number, add it to operation
+        if (operation.num1 != '' && operation.num2 === ''){
+            display.innerHTML = '';
+            console.log('RESET')
+        }    
+        display.innerHTML += button.id;
             array.push(button.id)
-            console.log(array)
-            // if an operand, stop
     })})
 
 //input operator and update 1st value
 operatorButtons.forEach((button) => {
     button.addEventListener('click',(event) => {
-        if (array !== []) {
+        if (operation.num1 === '') {
+            //memorizes 1st value and operator 
             operation.num1 = Number(array.join(""));
+            operation.operator = button.id
             array = [];
             display.innerHTML = ""
-            operation.operator = button.id
-            console.log(operation.num1)
-            console.log(operation.operator)
+        } else {
+            getTotal();
+             operation.operator = button.id
+             
         }
     } )
 })
 
-//
+//equal button 
+const equalButton = document.querySelector("#equal");
+equalButton.addEventListener('click', (event) => {
+    getTotal();
+})
+
+const getTotal = () => {
+//keeps 2nd value
+    operation.num2 = Number(array.join(""));
+    let result = operate(operation.num1, operation.num2, operation.operator)
+        resetData();
+        display.innerHTML = roundResult(result)
+        operation.num1 = result;
+        console.log(operation)
+}
+
